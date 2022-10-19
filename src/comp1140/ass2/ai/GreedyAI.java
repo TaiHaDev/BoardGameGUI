3c2b88be9abc6fa11df46f31bd95b99ff4c1c70c
Matthew Richards <mrichards30630@gmail.com>
2022-10-19 23:06:17 +1100

    fix greedy AI
    problem was comparing players' equality instead of just their unique ID

--- src/comp1140/ass2/ai/GreedyAI.java
+++ src/comp1140/ass2/ai/GreedyAI.java
@@ -12,17 +12,18 @@ public record GreedyAI(GameInstance game, Player player) implements AIPlayer {
 
     @Override
     public double evaluate() {
-        return evaluate( game.getAsEncodedString());
+        return evaluate(game.getAsEncodedString());
     }
 
     public double evaluate(String boardState) {
         GameInstance game = new GameInstance(boardState);
         return 5 * game.getBoard().getKnightBoard().values().stream().filter(knight -> player.equals(knight.getOwner())).filter(Knight::isJoker).count()
                 + 2.5 * player.getScore()
-                + game.getBoard().getResidentialBuilding().values().stream().filter(building -> player.equals(building.getOwner())).count()
-                + Stream.of(game.getBoard().getRoads()).filter(building -> player.equals(building.getOwner())).count()
+                + game.getBoard().getResidentialBuilding().values().stream().filter(building -> building.getOwner() != null && player.getUniqueId().equals(building.getOwner().getUniqueId())).count()
+                + Stream.of(game.getBoard().getRoads()).filter(building -> building.getOwner() != null && player.getUniqueId().equals(building.getOwner().getUniqueId())).count()
                 + 2 * CatanDiceExtra.longestRoad(game.getAsEncodedString())[player.getUniqueId().charAt(0) - 'W'];
     }
+
     @Override
     public String[] selectActionSequence() {
         String boardState = game.getAsEncodedString();
