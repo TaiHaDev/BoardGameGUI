ce0716a14c18681c550a5f40e42026b308e948a1
Matthew Richards <mrichards30630@gmail.com>
2022-10-19 22:31:40 +1100

    wip mcts

--- src/comp1140/ass2/ai/MonteCarloAI.java
+++ src/comp1140/ass2/ai/MonteCarloAI.java
@@ -1,14 +1,26 @@
 package comp1140.ass2.ai;
 
 import comp1140.ass2.CatanDiceExtra;
+import comp1140.ass2.buildings.Knight;
 import comp1140.ass2.gameobjects.GameInstance;
 import comp1140.ass2.gameobjects.Player;
+
+import java.util.stream.Stream;
 
 public record MonteCarloAI(GameInstance game, Player player) implements AIPlayer {
 
     @Override
     public double evaluate() {
-        return 0;
+        return evaluate( game.getAsEncodedString());
+    }
+
+    public double evaluate(String boardState) {
+        GameInstance game = new GameInstance(boardState);
+        return 5 * game.getBoard().getKnightBoard().values().stream().filter(knight -> player.equals(knight.getOwner())).filter(Knight::isJoker).count()
+                + 2.5 * player.getScore()
+                + game.getBoard().getResidentialBuilding().values().stream().filter(building -> player.equals(building.getOwner())).count()
+                + Stream.of(game.getBoard().getRoads()).filter(building -> player.equals(building.getOwner())).count()
+                + 2 * CatanDiceExtra.longestRoad(game.getAsEncodedString())[player.getUniqueId().charAt(0) - 'W'];
     }
 
     @Override
