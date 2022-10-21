df6347dbdcfc3d8127ae431dacc484ef0d1e75b6
Matthew Richards <mrichards30630@gmail.com>
2022-10-21 13:08:39 +1100

    fix ai not keeping correctly

--- src/comp1140/ass2/ai/AIFighterUtil.java
+++ src/comp1140/ass2/ai/AIFighterUtil.java
@@ -1,32 +1,49 @@
 package comp1140.ass2.ai;
 
+import comp1140.ass2.CatanDiceExtra;
+import comp1140.ass2.game.Resource;
 import comp1140.ass2.gameobjects.GameInstance;
 import comp1140.ass2.gameobjects.Player;
 
 import java.util.List;
+import java.util.Map;
 
 public class AIFighterUtil {
 
     public static void main(String[] args) throws InterruptedException {
-        GameInstance game = new GameInstance("W00WXYW00X00Y00");
-        AIPlayer guy1 = new SmartNoExpAI(game.getPlayers().get(0));
-        game.getPlayers().get(0).setDisplayName("Semi-Smart AI");
+        GameInstance game = new GameInstance("W00WXW00X00");
+        AIPlayer guy1 = new GreedyAI(game.getPlayers().get(0));
+        game.getPlayers().get(0).setDisplayName("Greedy AI");
 
         AIPlayer guy2 = new SmartAI(game.getPlayers().get(1));
         game.getPlayers().get(1).setDisplayName("Smart AI");
 
-        AIPlayer guy3 = new GreedyAI(game.getPlayers().get(2));
-        game.getPlayers().get(2).setDisplayName("Greedy AI");
-
         while (game.getPlayers().stream().noneMatch(player -> player.getScore() >= 10)) {
-            for (AIPlayer ai : new AIPlayer[] { guy1, guy2, guy3 }) {
+            for (AIPlayer ai : new AIPlayer[] { guy1, guy2 }) {
                 if (game.getCurrentPlayer().getUniqueId().equals(ai.player().getUniqueId())) {
                     String[] sequence;
+                    if (game.getRollsDone() == 0) {
+                        game.rollDice(game.getDiceCount());
+                    }
                     while (game.getRollsDone() < 3 && game.getDiceCount() != 0) {
                         game.setRollsDone(game.getRollsDone() + 1);
-                        game.rollDice(game.getDiceCount());
                         sequence = ai.selectActionSequence(game.getAsEncodedString());
                         game.applyActionSequence(sequence);
+
+                        if (sequence.length > 0 && sequence[0].startsWith("keep")) {
+                            int sum = 0;
+                            for (int roll : game.getDiceResult().values()) {
+                                sum += roll;
+                            }
+                            Map<Resource, Integer> result = game.getDiceResult();
+                            Map<Resource, Integer> reroll = GameInstance.stringResourcesToMap(CatanDiceExtra.rollDice(game.getDiceCount() - sum));
+                            for (Resource resource : Resource.values()) {
+                                int n = result.getOrDefault(resource, 0) + reroll.getOrDefault(resource, 0);
+                                if (n > 0) result.put(resource, n);
+                            }
+                            game.setDiceResult(result);
+                        }
+
                         System.out.println("Player " + game.getCurrentPlayer().getUniqueId() + " chose " + List.of(sequence));
                     }
                     sequence = ai.selectActionSequence(game.getAsEncodedString());
